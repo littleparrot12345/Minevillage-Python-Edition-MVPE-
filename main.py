d37c012f8cc1ecb85594cf7658bbe5967d0ff549
littleparrot12345 <littleparrot123@qq.com>
2023-12-05 00:29:56 +0800

    1.1.2.16
    添加了图标
    现在会渲染方块

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-VER="1.1.1.7"
+VER="1.1.2.16"
 from tkinter import *
 from time import sleep
-from os import getcwd
+from os import getcwd,path
 data={}
 def data_save():
     global data
@@ -13,26 +13,30 @@ def data_load():
     lf=open("data.mvpes","rb")
     data=pickle.load(lf)
     lf.close()
-mx=None
-my=None
+mx=0
+my=0
 cwd=getcwd()
-bt={"grass":["\.minevillage\mvt\gbtc.png"]}
-class block():
+bt={"grass":["/.minevillage/mvt/gbtc.png"]}
+canves=None
+gm=[]
+class Block():
     t="grass"
     sel=False
-    x=None
-    y=None
+    x=0
+    y=0
     ci=0
-    cim=0
-    def Tick():
-        if(mx>=x and mx<=x+16 and my>=x and my<=y+16):
+    cim=1
+    def tick(self):
+        global mx,my
+        if(mx>=self.x and mx<=self.x+16 and my>=self.y and my<=self.y+16):
             self.sel=True
         else:
             self.sel=False
         self.ci+=1
         self.ci%=self.cim
-        
-def print_pos(event):
+    def draw(self):
+        canves.create_image(self.x,self.y,image=bt[self.t][self.ci],anchor=NW)
+def mp(event):
     global mx,my
     mx,my=event.widget.winfo_pointerxy()
 def Tick():
@@ -40,15 +44,34 @@ def Tick():
     return
 def li(s):
     for i in range(len(bt[s])):
-        bt[s][i]=PhotoImage(cwd+bt[s][i])
-tk=Tk() 
+        img_path=cwd+bt[s][i]
+        bt[s][i]=PhotoImage(file=img_path)
+tk=Tk()
+tk.iconbitmap(cwd+"/.minevillage/icons/icon.ico")
 tk.title("Minevillage "+VER)
 canves=Canvas(tk,width=1024,height=576,highlightthickness=0)
 canves.pack()
 tk.update()
-tk.bind("<Motion>",print_pos)
+tk.bind("<Motion>",mp)
+for i in ["grass"]:
+    li(i)
+for i in range(0,1024,16):
+    gm.append([])
+    xb=i//16
+    for j in range(0,576,16):
+        tmp=Block()
+        tmp.x=i
+        tmp.y=j
+        tmp.draw()
+        gm[xb].append(tmp)
+for row in gm:
+    for block in row:
+        block.draw()
 while True:
     sleep(1/20)
+    for row in gm:
+        for block in row:
+            block.tick()
     Tick()
     tk.update_idletasks()
     tk.update()
